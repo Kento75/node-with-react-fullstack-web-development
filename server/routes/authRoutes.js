c9dc0fb60eb34691583ee16770f8a691b501a764
Kento75 <kento2github@gmail.com>
2019-04-12 06:10:00 +0900

    Add logout route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,15 @@ module.exports = (app) => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  // ログアウト
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  // ユーザー情報
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
