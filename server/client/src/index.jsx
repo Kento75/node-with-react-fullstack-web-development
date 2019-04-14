d68247f6ec500fd3b25966b4e5a87724342915cf
Kento75 <kento2github@gmail.com>
2019-04-14 09:53:19 +0900

    Add client store and base compoennts

--- server/client/src/index.jsx
+++ server/client/src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render (<App />, document.getElementById ('root'));
+import App from './components/App';
+
+const store = createStore (() => {}, applyMiddleware ());
+
+ReactDOM.render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector ('#root')
+);
