a6f2462f7c92acb9c0ee9b75381eef996d4f901c
Kento75 <kento2github@gmail.com>
2019-04-16 06:20:51 +0900

    Add Header component and Route component

--- server/client/src/components/App.jsx
+++ server/client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -11,7 +11,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" component={Landing} />
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
