487d24b201c5f0c22240f9358b0c8f6fd79e36bf
Kento75 <kento2github@gmail.com>
2019-04-20 10:26:57 +0900

    Add fetch login actions

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    const data = this.props.auth === null ? null : this.props.auth.data.length;
+    switch (data) {
+      case null:
+        return (
+          <li>
+            <a href="/account/login">loading</a>
+          </li>
+        );
+      case 0:
+        return (
+          <li>
+            <a href="/account/login">login</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/account/login">logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +33,15 @@ class Header extends React.Component {
           <a className="left brand-logo" href="#">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a href="/auth/google">Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({auth}) => {
+  return {auth};
+};
+
+export default connect(mapStateToProps)(Header);
