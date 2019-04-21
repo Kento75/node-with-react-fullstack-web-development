202dc1e7e5c93809dc94999fafa19b009a4f3cfa
Kento75 <kento2github@gmail.com>
2019-04-21 14:10:05 +0900

    Fix heroku deploy command and Added survey schema

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
+// MongoDBモデル
 require('./models/User');
+require('./models/Survey');
+
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, {
@@ -25,7 +28,6 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -33,6 +35,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 // stripeルート
 require('./routes/billingRoutes')(app);
+
+// 本番環境はフロントエンド用ルートを実装
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ server/models/Survey.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {
+  Schema
+} = mongoose;
+
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [String]
+});
+
+mongoose.model('surveys', surveySchema);
