a0cb5bdddac87b5a1ad35931b238934738bf0a70
Kento75 <kento2github@gmail.com>
2019-04-27 15:36:06 +0900

    Add redux-form and surveyFrom component

---
+++ server/client/src/components/surveys/SurveyNew.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import SurveyForm from './SurveyForm';
+
+export default class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
+  }
+}
