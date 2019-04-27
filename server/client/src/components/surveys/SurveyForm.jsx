b6680ac783dc956760b6e9e0592c188d7ddbc0e1
Kento75 <kento2github@gmail.com>
2019-04-27 19:27:20 +0900

    Add survey form filed

--- server/client/src/components/surveys/SurveyForm.jsx
+++ server/client/src/components/surveys/SurveyForm.jsx
@@ -1,9 +1,38 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  {label: 'Survey Title', name: 'title'},
+  {label: 'Subject Line', name: 'subject'},
+  {label: 'Email Body', name: 'body'},
+  {label: 'Recipient List', name: 'emails'},
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({label, name}) => {
+      return (
+        <Field
+          key={label}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
   render() {
-    return <div>SurveyForm</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
