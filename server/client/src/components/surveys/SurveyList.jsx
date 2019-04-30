f6b73771f88baf008352c0cc3c0b4f74e5d402b7
Kento75 <kento2github@gmail.com>
2019-04-30 19:09:14 +0900

    Add show send mail list

--- server/client/src/components/surveys/SurveyList.jsx
+++ server/client/src/components/surveys/SurveyList.jsx
@@ -6,8 +6,30 @@ class SurveyList extends React.Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+
+  // 送信メールを一覧で表示する
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div key={survey._id} className="card  darken-1">
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
